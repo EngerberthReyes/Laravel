eeeaeee646b1963100c54e9db3fbb457667c1808
Engerberth Reyes <engerberthr73@gmail.com>
2026-05-02 01:01:25 -0400

    ae

--- resources/views/sistema_de_interfaces/index.blade.php
+++ resources/views/sistema_de_interfaces/index.blade.php
@@ -2,10 +2,19 @@
 
 @section('contenido')
 
-<table>
-    @foreach($interfaces as $interfaz)
-    <p>{{ $interfaz->nombre }}</p>
-    @endforeach
+<table class="table">
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+        </tr>
+        @foreach($interfaces as $interfaz)
+        <tr>
+            <td>{{ $interfaz->id }}</td>
+            <td>{{ $interfaz->nombre }}</td>
+        </tr>
+        @endforeach
+    </table>
 </table>
 
 @endsection
